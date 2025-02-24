87e094176876d22c937fcbe03a0d70c379c182ce
Joan Nyawira Kori <korijoan171@gmail.com>
2025-02-24 20:53:24 +0300

    Modifies index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//declaring the shout(string) function
+function shout(string) {
+     return string.toUpperCase();//returns a string in uppercase
+}
+function whisper(string){
+     return string.toLowerCase();
+}
+function logShout(string){
+     const spy="HELLO";
+     console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase()); 
+}
+function sayHiToHeadphonedRoommate(string){
+     if(string === string.toLowerCase()){
+          return "I can't hear you!";
+     }
+          else if(string === string.toUpperCase()){
+               return "YES INDEED!";
+          }
+               else if(string === "Let's have dinner together!"){
+                    return "I would love to!";
+               }
+          }
